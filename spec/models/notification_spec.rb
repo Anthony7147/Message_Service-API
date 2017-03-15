42cd967bfee0a2565ba03f9c892ecdd867e9c8cd
Anthony <Knoll>
2017-03-15 15:58:13 -0600

    Validations in rails models

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  describe 'creation' do
-    it 'can be created' do
+  describe 'validations' do
+    it 'can be created if valid' do
       notification = FactoryGirl.build_stubbed(:notification)
-      expect(notification).to be_valid
+      notification.phone = nil
+      notification.body = nil
+      notification.source_app = nil
+      expect(notification).to_not be_valid
     end
   end
 end
