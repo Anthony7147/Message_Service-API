0028378db9c7581ff92139192ec199592f5efc59
Anthony <Knoll>
2017-03-16 14:18:01 -0600

    Ensuring Data Integrity with ActiveRecord Validations

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,13 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  describe 'validations' do
-    it 'can be created if valid' do
+  describe 'creation' do
+    it 'can be created' do
       notification = FactoryGirl.build_stubbed(:notification)
-      notification.phone = nil
-      notification.body = nil
-      notification.source_app = nil
-      expect(notification).to_not be_valid
+      expect(notification).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    before { @notification = FactoryGirl.build_stubbed(:notification) }
+
+  	it 'can be created if valid' do
+      @notification.phone = nil
+      @notification.body = nil
+      @notification.source_app = nil
+      expect(@notification).to_not be_valid
+    end
+
+    it 'requires the phone attribute to contain a string of integers' do
+      @notification.phone = "atextphonenumber"
+      expect(@notification).to_not be_valid
+    end
+
+    it 'requires the phone attribute to only have 10 characters' do
+      @notification.phone = "12345678901"
+      expect(@notification).to_not be_valid
+    end
+
+    it 'limits the body attribute to 160 characters' do
+      @notification.body = "word" * 500
+      expect(@notification).to_not be_valid
     end
   end
 end
